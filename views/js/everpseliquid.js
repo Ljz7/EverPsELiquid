fcb845ce48f56eadf007d0c2fd11b65593d05cd8
Ljz7 <aleber@team-ever.com>
2017-05-26 17:03:12 +0400

    Correction indentation views/js/everpseliquid.js

--- views/js/everpseliquid.js
+++ views/js/everpseliquid.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
             $('#booster').html(calculation.booster);
             $('#base').html(calculation.base);
         }
-	});
+    });
 });
 
 function calculateBoostAndBase(quantity, nicotine) {
